1c1117613532b5113db6ed44ba32ab6eb0092c38
Lance <lance.james08@gmail.com>
2022-07-15 16:31:16 +1000

    UpdateAdd project files.

---
+++ BlogDemo/Features/As a StockApp User I want to purchase a given.feature
@@ -0,0 +1,12 @@
+﻿Feature: As a StockApp User I want to purchase a given
+
+A short summary of the feature
+
+@tag1
+@DataSource:StockValues.xlsx
+Scenario: As a StockApp user I want to purchase a given amount of stock at the latest value so that I can increase the value of my portfolio
+	Given That I am a StockApp User
+	And My initial portfolio has a value of "<InitialPortfolioValue>"
+	When I purchase "<AmountStockToBuy>" amount of "<StockCode>" at the latest value
+	Then My portfolio "<InitialPortfolioValue>" has increased in value
+
---
+++ StockApp/StockAppClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp
+{
+    public class StockAppClass
+    {
+        public string? User { get; set; }
+        public int PortfolioValue { get; set; }
+
+        public string JSonValue { get; set; }
+
+        public void SetJSonValue(string JSonVal)
+        {
+            JSonValue += JSonVal + "\n";
+        }
+        public void SetPortfolioValue(int value)
+        {
+            PortfolioValue = value;
+        }
+
+        public void PurchaseStock(string amount, string stockCode)
+        {
+            int latestPrice = GetLatestPrice(stockCode);
+            int netSumToAdd = Int32.Parse(amount) * latestPrice;
+            PortfolioValue += netSumToAdd;
+        }
+
+        private int GetLatestPrice(string stockCode)
+        {
+            switch (stockCode)
+            {
+                case "MSFT":
+                    return 287;
+                case "SBUX":
+                    return 93;
+                case "AMZN":
+                    return 3052;
+                case "BA":
+                    return 209;
+                case "ZAR":
+                    return 520;
+                case "ABN":
+                    return 250;
+                case "AUD":
+                    return 155;
+                case "FOR":
+                    return 275;
+                case "TUX":
+                    return 75;
+                default:
+                    return 0;                
+            }
+        }
+    }
+}
